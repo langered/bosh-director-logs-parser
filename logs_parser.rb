bc123f9b630edec0997a15c53cdd262eb729634b
Denis Langer <denis.langer@sap.com>
2019-02-11 17:45:18 +0100

    Colorize messages and show dropped messages
    Co-authored-by: Vladimir Videlov <vladimir.videlov@sap.com>

--- logs_parser.rb
+++ logs_parser.rb
@@ -40,7 +40,8 @@ File.readlines(debug_file_path).each do |line|
         messages[subject] = { req_id: subject,
             timestamp: time,
             total_sent: total_sent,
-            open_sent: open_sent }
+            open_sent: open_sent,
+            method: method }
         #received
       else
         unless messages[subject].nil?
@@ -58,11 +59,11 @@ File.readlines(debug_file_path).each do |line|
 end
 
 file = File.open("messages.csv", 'w')
-file.write "req_id,timestamp,duration,total_sent,open_sent\n"
+file.write "req_id,timestamp,duration,total_sent,open_sent,method\n"
 
 messages.values.each do |elem|
   if elem[:duration]
-    file.write "#{elem[:req_id]},#{elem[:timestamp].strftime(DATETIME_FORMAT)},#{elem[:duration]},#{elem[:total_sent]},#{elem[:open_sent]}\n"
+    file.write "#{elem[:req_id]},#{elem[:timestamp].strftime(DATETIME_FORMAT)},#{elem[:duration]},#{elem[:total_sent]},#{elem[:open_sent]},#{elem[:method]}\n"
   else
     dropped_messages[elem[:timestamp]] ||= 0
     dropped_messages[elem[:timestamp]] += 1
--- plot.R
+++ plot.R
@@ -3,21 +3,37 @@ args = commandArgs(trailingOnly=TRUE)
 pdf(args[3])
 
 data <- read.csv(file=args[1], header=TRUE, sep=",")
+data$color="black"
+
+data$color[data$method=="get_task"]="gray"
+data$color[data$method=="get_state"]="red"
+data$color[data$method=="update_settings"]="blue"
+data$color[data$method=="drain"]="brown"
+data$color[data$method=="start"]="green"
+data$color[data$method=="stop"]="purple"
+data$color[data$method=="apply"]="gold"
+data$color[data$method=="run_script"]="deeppink"
+data$color[data$method=="upload_blob"]="orange"
+
 data$timestamp = strptime(data$timestamp, "%Y-%m-%dT%H:%M:%OS")
-
-attach(mtcars)
-par(mfrow=c(2,2))
-
-timeDescription = sprintf("from=\"%s\"\nto=\"%s\"", data$timestamp[1], tail(data$timestamp, n=1))
-plot(data$timestamp, data$duration, xlab=timeDescription, ylab='duration in ms')
-plot(data$timestamp, data$total_sent, xlab=timeDescription, ylab='total sent')
-plot(data$timestamp, data$open_sent, xlab=timeDescription, ylab='open sent')
 
 dropped_messages <- read.csv(file=args[2], header=TRUE, sep=",")
 dropped_messages$timestamp = strptime(dropped_messages$timestamp, "%Y-%m-%dT%H:%M:%OS")
 
-timeDescriptionDroppedMessages = sprintf("from=\"%s\"\nto=\"%s\"", dropped_messages$timestamp[1], tail(dropped_messages$timestamp, n=1))
+attach(mtcars)
+par(mfrow=c(3,1))
 
-plot(dropped_messages$timestamp, dropped_messages$count, xlab=timeDescriptionDroppedMessages)
+timeDescription = sprintf("from=\"%s\" to=\"%s\"", data$timestamp[1], tail(data$timestamp, n=1))
+
+plot(data$timestamp, data$duration, xlab=timeDescription, col=data$color, ylab='duration in ms', xaxt='n')
+axis.POSIXct(1, data$timestamp, format="%H:%M:%OS")
+abline(v = as.POSIXct(dropped_messages$timestamp), col="lightpink")
+
+plot(data$timestamp, data$total_sent, xlab=timeDescription, col=data$color, ylab='total sent', xaxt='n')
+axis.POSIXct(1, data$timestamp, format="%H:%M:%OS")
+abline(v = as.POSIXct(dropped_messages$timestamp), col="lightpink")
+plot(data$timestamp, data$open_sent, xlab=timeDescription, col=data$color, ylab='open sent', xaxt='n')
+axis.POSIXct(1, data$timestamp, format="%H:%M:%OS")
+abline(v = as.POSIXct(dropped_messages$timestamp), col="lightpink")
 
 dev.off()
